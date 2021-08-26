0b1aec5925d4b5c94219b58e6cf317ff4988addc
Jack O'Dell <jack.odell@huskers.unl.edu>
2021-08-26 16:13:29 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Jack A. O'Dekk
+ * 
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jack O'Dell!\n");
+
+  return 0;
+}
